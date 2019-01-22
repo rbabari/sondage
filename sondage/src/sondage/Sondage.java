e02506e2b41aca6b7e255ccc4bfa84b0720941ea
RBabari <RBabari@LI-S024-0T01>
2019-01-22 10:54:19 -0500

    rajouter methode à la classe sondage2

--- sondage/src/sondage/Sondage.java
+++ sondage/src/sondage/Sondage.java
@@ -15,7 +15,7 @@ public class Sondage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here gg
     }
       public static void methode( ) {
           System.out.println("hello");
